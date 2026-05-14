99312d42a234976cd71c21ce68365be74ac6fbb3
Kasia Topór <kasiatopor@Mac.lan>
2026-05-14 17:21:09 +0200

    add routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Profile } from "./pages/Profile";
+import { MyReviews } from "./pages/MyReviews";
 
 
 export const router = createBrowserRouter([
@@ -9,6 +13,10 @@ export const router = createBrowserRouter([
     Component: Layout,
     children: [
       { index: true, Component: Home },
+      { path: "login", Component: Login },
+      { path: "register", Component: Register },
+      { path: "profile", Component: Profile },
+      { path: "my-reviews", Component: MyReviews },
     ],
   },
 ]);
