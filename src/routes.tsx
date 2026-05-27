ed96cb87059dea89c37c844d44217ee841bcca08
Weronika <weronikaw@remitly.com>
2026-05-27 11:13:57 +0200

    Added RestaurantDetails layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { AdminDashboard } from "./pages/AdminDashboard";
 import { RestaurantList } from "./pages/RestaurantList";
+import { RestaurantDetails } from "./pages/RestaurantDetails";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
       { index: true, Component: Home },
       { path: "admin", Component: AdminDashboard },
       { path: "restaurants", Component: RestaurantList },
+      { path: "restaurants/:id", Component: RestaurantDetails },
     ],
   },
 ]);
